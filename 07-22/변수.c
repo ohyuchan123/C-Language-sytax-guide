fab4797cc082ac4626164e39e176e05b31ebc07b
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2023-07-22 13:29:23 +0900

    c언어 변수

---
+++ 07-22/변수.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+
+int main(){
+
+    #pragma region 변수
+    //변수 : 변하는 숫자. 예기치 못한 샂건
+    //프로그래밍에서의 변수 : 어떤 데이터를 저장하는 저장 공간.(메모리)
+
+    //데이터 형식(자료형)
+    //메모리의 크기가 클 수록 더 많은 숫자를 표현할 수 있고 더 정확하다.
+    // - 정수(−32,767~32,767 즉, 소수점이 없는 숫자) : int(4바이트), short(2바이트), long(8바이트), long long(8바이트)
+    //      int : integer(정수) 4바이트
+    // - 실수(소수점이 있는 숫자 : float, double(8바이트)
+    //      float : float(뜨다, 부(뜨다)동 소수점 방식). 4바이트
+    // - 문자 : char
+    //      char : character(문자). 1바이트
+
+    #pragma endregion
+
+    #pragma region 변수 만들기(선언)
+    // <문법>
+    // [데이터 형식][변수의 이름];
+    // ex int number; //number라는 이름에 4바이트 크기의 정수 데이터 저장 공간을 만들었다.
+    // 자징 공간을 처음 만들면 그 저장 공간 안에는 '쓰레기 값'이 들어 있따.
+    // '쓰레기 값' : 쓸모없는 데이터.
+
+    // 변수에 데이터 넣기
+    // 변수에 최초로 데이터를 대입 : 변수 초기화
+    // ex. int number = 10; //number라는 저장 공간에 10이라는 정수 데이터를 대입
+    // 대(대신)입하다 : 
+
+    int number = 10;
+
+    // %d : 정수 변환 문자
+    printf("number = %d\n",number);
+    #pragma endregion
+
+    #pragma region 대입
+
+    int number_1;
+    number_1 = 10; //number라는 저장공간에 10을 대입한다.
+    number_1 = 48; //number라은 저장공간에 48을 대입한다.
+
+    number_1 = number_1 + 4;
+    printf("number = %d\n",number_1);
+
+    #pragma endregion
+
+    #pragma region 변수 출력
+    // 이름, 나이, 키 몸무게 출력
+    char name[20] = "홍길동"; //배열
+    int age = 19;
+
+    //소수점 숫자
+    // 소수점 숫자 뒤에 f를 붙임 : float
+    // 소수점 숫자 뒤에 f를 붙이지 않음 : double
+    // ※ 데이터 형식이 일치하지 않으면, 자동으로 변환이 일어난다.
+    // (컴퓨터가 변환 작업을 더 하기 때문에 느려진다.)
+
+    /*
+        ex) float height = 180.5f;
+    */
+    double height = 180.5 ,weight = 68.7;
+
+    printf("나의 이름은 %s 이고 현재 나이는 %d살이야\n",name,age);
+    printf("현재 나의 키는 %.2lfcm 이고 몸무게는 %.2lfkg 이야.",height,weight);
+
+    /* <변환 문자>
+        %d : 정수 변환 문자
+        %f : 실수 변환 문자
+        %lf : 실수 변환 문자
+            %.2f : 소수점 2번째 자리까지 출력
+        %c : 문자 변환 문자
+        %s : 문자열 변환 문자
+    */
+
+
+    int number1,number2;
+    scanf("%d %d",&number1,&number2);
+    printf("%d + %d = %d\n",number1,number2,number1+number2);
+    #pragma endregion
+    return 0;
+}
